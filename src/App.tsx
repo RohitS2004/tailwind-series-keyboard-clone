8d7b88a24aad604f9c62c37c28a6e00dd58823bc
Rohit Saini <rohit.boby2004@gmail.com>
2026-02-12 19:22:54 +0530

    Removed the unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
 import { Container, Keyboard } from "./components";
-import hpData from "../hp-compact-350.json";
-import { useEffect, useState } from "react";
 
 const App = () => {
-  const [model, setModel] = useState<string>();
-  const [keyCount, setKeyCount] = useState<number>();
-
-  useEffect(() => {
-    setModel(hpData.model);
-    setKeyCount(hpData.keyCount);
-  }, []);
-
   return (
     <div className="min-h-screen max-w-full bg-neutral-900">
       <Container>
